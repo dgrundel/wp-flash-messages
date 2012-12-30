fe5d029197e2f06e2a32733defbe79747c8ac9d6
Daniel Grundel <daniel@webpresencepartners.com>
2012-12-30 14:33:04 -0500

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ wp-flash-messages
 =================
 
 Simple flash message functionality for your WordPress plugin.
+
+Use the function *queue_flash_message()* to enqueue a flash message that will be displayed on the next admin page load.
+
+*queue_flash_message()* takes the following form:
+
+```
+<?php queue_flash_message( $message, $class = 'updated' ) ?>;
+```
+
+The two parameters are:
+
+-- $message - The string to be displayed. HTML is okay, but keep in mind that your $message will be wrapped in a *p* and a *div* tag.
+-- $class - the CSS class to be applied to the div element.
+
+The output will look like this:
+
+```
+<div class="$class">
+	<p>$message</p>
+</div>
+```
+
+*This is the same markup that WordPress generates for its own flash messages.*
---
+++ wp-flash-messages.php
@@ -0,0 +1,51 @@
+<?php /*
+    Plugin Name: WP Flash Messages
+    Plugin URI: http://webpresencepartners.com
+    Description: Easily Show Flash Messages in WP Admin
+    Version: 1
+    Author: Daniel Grundel, Web Presence Partners
+    Author URI: http://webpresencepartners.com
+*/
+
+	class WPFlashMessages {
+
+		public function __construct() {
+		    add_action('admin_notices', array(&$this, 'show_flash_messages'));
+		}
+	
+        //Flash Messages
+        public static function queue_flash_message($message, $class = null) {
+        	$class = ($class === null) ? 'updated' : $class;
+            $flash_messages = maybe_unserialize(get_option('wp_flash_messages', ''));
+            
+            if(!is_array($flash_messages)) $flash_messages = array();
+            if(!is_array($flash_messages[$class])) $flash_messages[$class] = array();
+            
+            $flash_messages[$class][] = $message;
+            
+            update_option('wp_flash_messages', serialize($flash_messages));
+        }
+        public static function show_flash_messages() {
+            $flash_messages = unserialize(get_option('wp_flash_messages', serialize('')));
+            
+            if(is_array($flash_messages)) {
+                foreach($flash_messages as $class => $messages) {
+                    foreach($messages as $message) {
+                        ?><div class="<?php echo $class; ?>"><p><?php echo $message; ?></p></div><?php
+                    }
+                }
+            }
+            
+            //empty out flash messages
+            update_option('wp_flash_messages', serialize(''));
+        }
+	}
+
+	new WPFlashMessages();
+
+	//convenience function
+	if( class_exists('WPFlashMessages') && !function_exists('queue_flash_message') ) {
+		function queue_flash_message($message, $class = null) {
+			WPFlashMessages::queue_flash_message($message, $class);
+		}
+	}
